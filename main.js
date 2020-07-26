20c83b51c0823a8c96d839e9815168c8c9748fa0
Anthony-cpp <54949075+Anthony-cpp@users.noreply.github.com>
2020-07-27 02:17:33 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var event_num=0;
+
+function(event_num){
+	if(event_num == 0){
+
+//‚±‚±‚Éˆ—‚ğ‘‚­
+
+	event_num = 1;
+	}
+else if(event_num == 1){
+	document.getElementById("event_str").textContent = "TEST TEST";
+}
+else if(event_num == 2){
+
+	//‚±‚±‚Éˆ—‚ğ‘‚­
+}
+	
